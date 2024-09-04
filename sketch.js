f863bf8c93c2713d4ae5ff316006f2d950bab4b9
Dajohnson000 <daj22c@fsu.edu>
2024-09-04 19:07:01 -0400

    rectangle bounces

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 let x1 = 100;     // x position of the ball
 let y1 = 0;       // y position of the ball
 let x1Speed = 3;  // speed in X direction
-let y2Speed = 7; // speed in Y direction
+let y1Speed = 7; // speed in Y direction
 let d1 = 20;      // diameter of the ball
-
+let x2 = 50
+let y2 = 20
+let x2Speed = 2
+let y2Speed = 5
+let d2 = 30
 function setup() {
   createCanvas(800, 800);
 }
@@ -20,9 +24,21 @@ function draw() {
   
   // same as above, but for the top and bottom of the canvas
   if (y1 > width || y1 < 0) {
-    y2Speed = y2Speed * -1;  
+    y1Speed = y1Speed * -1;  
   }
-  y1 = y1 + y2Speed;
+  y1 = y1 + y1Speed;
 
   ellipse(x1, y1, d1);
+
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1; 
+  }
+  x2 = x2 + x2Speed;
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;
+  }
+  y2 = y2 + y2Speed
+
+  rect(x2, y2, d2);
+
 }
